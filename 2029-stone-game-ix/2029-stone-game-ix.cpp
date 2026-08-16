d6c703343933affa64f108f5536cbf2f852ec1d0
Anshika Chauhan <anshika10chauhan08@gmail.com>
2026-08-16 17:57:14 +0530

    Time: 1 ms (82.74%), Space: 131.3 MB (33.5%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.cpp
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     bool stoneGameIX(vector<int>& stones) {
 
-        int count[3] = {0, 0, 0};
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
 
-        // Count stones according to remainder when divided by 3
-        for (int x : stones) {
-            count[x % 3]++;
-        }
+        for (int i : stones) {
+            int x = i % 3;
 
-        // If number of 0-remainder stones is even
-        if (count[0] % 2 == 0) {
-            if (count[1] > 0 && count[2] > 0) {
-                return true;
+            if (x == 0) {
+                cnt0++;
             }
-            return false;
+            else if (x == 1) {
+                cnt1++;
+            }
+            else {
+                cnt2++;
+            }
         }
 
-        // If number of 0-remainder stones is odd
-        if (abs(count[1] - count[2]) > 2) {
-            return true;
+        if (cnt0 % 2 == 0) {
+            return cnt1 >= 1 && cnt2 >= 1;
         }
 
-        return false;
+        return abs(cnt1 - cnt2) > 2;
     }
 };
