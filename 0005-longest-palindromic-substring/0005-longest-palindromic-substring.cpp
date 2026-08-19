e1d04dd8836fd399de220c2d8b9573f66ca09a16
Anshika Chauhan <anshika10chauhan08@gmail.com>
2026-08-19 23:19:44 +0530

    Time: 0 ms (100%), Space: 11.4 MB (46.07%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string t = "^";
+
+        for (char c : s) {
+            t += "#";
+            t += c;
+        }
+
+        t += "#$";
+
+        int n = t.length();
+        vector<int> p(n, 0);
+
+        int center = 0, right = 0;
+        int maxLen = 0, maxCenter = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+
+            int mirror = 2 * center - i;
+
+            if (i < right)
+                p[i] = min(right - i, p[mirror]);
+
+            // Expand around center
+            while (t[i + (1 + p[i])] == t[i - (1 + p[i])]) {
+                p[i]++;
+            }
+
+            // Update center and right
+            if (i + p[i] > right) {
+                center = i;
+                right = i + p[i];
+            }
+
+            // Store longest palindrome
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                maxCenter = i;
+            }
+        }
+
+        int start = (maxCenter - maxLen) / 2;
+
+        return s.substr(start, maxLen);
+    }
+};
